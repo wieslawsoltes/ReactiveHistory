c579f41ac7d718d00d3338e197e1a2b26b731ace
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-09-01 00:31:44 +0200

    Added wpf sample

---
+++ ReactiveHistorySample.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Windows;
+
+namespace ReactiveHistorySample.Wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
