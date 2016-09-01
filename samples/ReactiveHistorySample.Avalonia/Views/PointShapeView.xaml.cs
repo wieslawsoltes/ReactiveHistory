e36227e99bd690e04ece1a8506d02d3b1725fd67
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-09-02 00:22:24 +0200

    Added view controls

---
+++ samples/ReactiveHistorySample.Avalonia/Views/PointShapeView.xaml.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ReactiveHistorySample.Avalonia.Views
+{
+    public class PointShapeView : UserControl
+    {
+        public PointShapeView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
